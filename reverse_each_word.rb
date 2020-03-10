c95d6d51fbaaab33e0047ca0139636ce307ddbb7
Crystal Schooling <bytesizemagic@gmail.com>
2020-03-10 06:43:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -11,3 +11,9 @@ end
 
 # takes sentence and returns each word reversed in place (using collect)
 def reverse_each_word(sentence)
+  sentence_split = sentence.split(" ")
+  reversed_sentence = sentence_split.collect do |word|
+    word.reverse
+  end
+  reversed_sentence.join(" ")
+end
