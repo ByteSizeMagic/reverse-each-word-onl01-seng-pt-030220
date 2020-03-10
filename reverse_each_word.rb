a7c0e398da15eb22b8ec3decfb134a3a561884ea
Crystal Schooling <bytesizemagic@gmail.com>
2020-03-10 06:34:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,7 @@
+# takes sentence and returns each word reversed in place (using each)
 def reverse_each_word(sentence)
   sentence.reverse
 end
+
+
+# takes sentence and returns each word reversed in place (using collect)
